e5c443de4d24a45692c73cad85695da85000ee86
B.T. Franklin <brandon.franklin@gmail.com>
2025-03-21 21:20:08 -0700

    Moved job seeker unit tests into a "unit" subfolder of "tests"

---
+++ apps/job_seekers/tests/unit/__init__.py
@@ -0,0 +1,6 @@
+"""
+Unit tests for the job_seekers app.
+
+This package contains unit tests that verify individual components
+of the job_seekers app in isolation from other system components.
+"""
