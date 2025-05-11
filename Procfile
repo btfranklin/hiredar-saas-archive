00bb03f6d9e85d6d8cfc055904ed15aa3904c746
B.T. Franklin <brandon.franklin@gmail.com>
2025-05-10 21:36:53 -0700

    Update environment variable naming and remove Procfile
    - Changed the environment variable from DB_USER to DB_USERNAME
    - Updated settings.py to reference the new DB_USERNAME variable while maintaining backward compatibility.
    - Removed the Procfile as it is no longer needed for the current deployment setup.

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: gunicorn hiredar.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 4
-worker:  python manage.py qcluster
-release: npm run build:css && python manage.py migrate && python manage.py collectstatic --noinput
--- hiredar/settings.py
+++ hiredar/settings.py
@@ -187,7 +187,7 @@ DATABASES = {
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
         "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
+        "USER": os.getenv("DB_USERNAME") or os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
     }
 }
