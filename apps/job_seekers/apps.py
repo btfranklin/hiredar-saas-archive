5682679133dcefe557a5d34599ba0c36621fe348
B.T. Franklin <brandon.franklin@gmail.com>
2025-10-14 18:36:22 -0700

    Remove unused ready method and signal initialization from JobSeekersConfig

--- apps/job_seekers/apps.py
+++ apps/job_seekers/apps.py
@@ -13,12 +13,3 @@ class JobSeekersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.job_seekers"
     verbose_name = "Job Seekers"
-
-    def ready(self) -> None:
-        """
-        Initialize signals when the app is ready.
-
-        This imports the signals module to ensure signal handlers are registered.
-        """
-        # pylint: disable=import-outside-toplevel,unused-import
-        import apps.job_seekers.signals  # noqa
